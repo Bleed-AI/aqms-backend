e9cc5ea37cae75bb6627f66471dbebaa809f03fa
Abdullah Raheel <Abdullaharaheel@gmail.com>
2023-08-09 13:04:51 +0500

    password script

---
+++ update_password_hash.py
@@ -0,0 +1,14 @@
+
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def validate_password(password, hashed_password):
+    return pwd_context.verify(password, hashed_password)
+
+
+def generate_password_hash(password):
+    return pwd_context.hash(password)
+
+print(generate_password_hash("admin"))
